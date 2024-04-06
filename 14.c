ffb1e2ccfbb5544a21a02e4f02016e3085650d9f
vivekrathod1111 <166017505+vivekrathod1111@users.noreply.github.com>
2024-04-06 19:01:12 +0530

    Add files via upload

---
+++ 14.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+#include <conio.h> 
+
+void display(int list[], int n) {
+    int i;
+    for(i = 0; i < n; i++)
+        printf("%d ", list[i]);
+}
+
+void selectionsort(int list[], int n) {
+    int indexMin, i, j, temp;
+    // loop through all numbers 
+    for(i = 0; i < n - 1; i++) {
+        // set current element as minimum 
+        indexMin = i;
+        // check the element to be minimum 
+        for(j = i + 1; j < n; j++) {
+            if(list[j] < list[indexMin])
+                indexMin = j;
+        }
+        if(indexMin != i) {
+            printf("\nItems swapped: [ %d, %d ]\n", list[i], list[indexMin]);
+            // swap the numbers 
+            temp = list[indexMin];
+            list[indexMin] = list[i];
+            list[i] = temp;
+        }
+        printf("\nIteration %d : ", (i + 1));
+        display(list, n);
+    }
+}
+
+int main() {
+    int list[100], i, n;
+   
+    printf("\nHow many elements do you want? : ");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+        printf("Enter the No [%d]: ", i + 1);
+        scanf("%d", &list[i]);
+    }
+    selectionsort(list, n);
+    printf("\n\nAfter Sorting:\n");
+    for(i = 0; i < n; i++)
+        printf("\t %d", list[i]);
+    getch();
+    return 0;
+}
---
+++ 16.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+#include<conio.h> 
+void main() 
+{ 
+int array[10],size,index,i; 
+clrscr(); 
+printf("\nEnter array size : "); 
+scanf("%d",&size); 
+for(i=0;i<size;i++) 
+{ 
+printf("\nEnter value of elements [%d] : ",i); 
+scanf("%d",&array[i]); 
+} 
+printf("\nEnter Index No to be Search : "); 
+scanf("%d",&index); 
+if(index>size) 
+printf("\nWrong Index No. Entered"); 
+else 
+printf("\nThe Value %d at Index possition is %d", array[index], index); 
+getch(); 
+}
